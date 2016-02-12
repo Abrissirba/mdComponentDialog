bb6a72f5dfd1b4d12926110e3eef9aa0e7bfbaeb
Marcus Abrahamsson <marcus.abrahamsson13@gmail.com>
2016-02-12 16:12:15 +0100

    Include dist

---
+++ dist/mdComponentDialog.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for mdComponentDialog
+// Project: https://github.com/Abrissirba/mdComponentDialog.git
+// Definitions by: Marcus Abrahamsson <https://github.com/abrissirba>
+
+/// <reference path="../angularjs/angular.d.ts" />
+/// <reference path="../angular-material/angular-material.d.ts" />
+
+declare module angular.mdComponentDialog {
+    interface IOptions extends angular.material.IDialogOptions  {
+        componentName: string;
+        title?: string;
+        params?: any;
+    }
+
+    interface IDialogService extends angular.material.IDialogService {
+        showAbrisComponentDialog(IOptions): angular.IPromise<any>;
+    }
+}
